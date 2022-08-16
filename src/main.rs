b67b49d95bec46f3596f886cefd8241db8ef5669
Canaan <canaanmckenzie@protonmail.com>
2022-08-16 17:38:34 -0400

    refactor with async/await

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-//standard parellism library for Rust
-use rayon::prelude::*;
-use reqwest::{blocking::Client, redirect};
-use std::{env, time::Duration};
+use futures::{stream, StreamExt};
+use reqwest::Client;
+use std::{
+    env,
+    time::{Duration,Instant},
+};
 
 mod error;
 pub use error::Error;
@@ -12,49 +14,45 @@ use model::Subdomain;
 mod common_ports;
 
 
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error>{
 
-fn main() -> Result<(), anyhow::Error>{
-
+    //collect input from terminal
     let args: Vec<String> = env::args().collect();
+
     if args.len() !=2 {
         return Err(Error::CliUsage.into());
     }
 
     let target = args[1].as_str();
 
+    //timeout if there is something like a firewall so it wont loop 
     let http_timeout = Duration::from_secs(15);
-    let http_client = Client::builder()
-        .redirect(redirect::Policy::limited(4))
-        .timeout(http_timeout)
-        .build()?;
-
-    //..
-    //custom threadpool to improve speed
-    let pool = rayon::ThreadPoolBuilder::new()
-        .num_threads(256)
-        .build()
-        .unwrap();
-
-
-    //pool.install is required to use custom threadpool
-    pool.install(|| {
-        let scan_result: Vec<Subdomain> = 
-            subdomains::enumerate(&http_client, target)
-                .unwrap()
-                .into_par_iter()
-                .map(ports::scan_ports)
-                .collect();
-
-        for subdomain in scan_result {
-            println!("{}", &subdomain.domain);
-            for port in &subdomain.open_ports{
-                println!("      {}", port.port);
-            }
-
-            println!("");
-        }
-    });
-
-    Ok(())
+    let http_client = Client::builder().timeout(http_timeout).build()?;
     
+    let ports_concurrency = 200;
+    let subdomains_concurrency = 100;
+    let scan_start = Instant::now();
+
+
+    let subdomains = subdomains::enumerate(&http_client, target).await?;
+
+    
+    let scan_result: Vec<Subdomain> = stream::iter(subdomains.into_iter())
+        .map(|subdomain| ports::scan_ports(ports_concurrency,subdomain))
+        .buffer_unordered(subdomains_concurrency)
+        .collect()
+        .await;
+
+    let scan_duration = scan_start.elapsed();
+    println!("Scan completed in {:?}",scan_duration);
+
+    for subdomain in scan_result{
+        println!("{}:",&subdomain.domain);
+        for port in &subdomain.open_ports{
+            println!("      {}: open",port.port);
+        }
+        println!("");
+    }
+    Ok(())
 }
